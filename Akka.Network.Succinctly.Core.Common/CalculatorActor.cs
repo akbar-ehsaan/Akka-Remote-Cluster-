3a1f3afbcb4fecf85a3bb496f2487a273d1770fa
Ehsan Akbar <45014174+akbar-ehsaan@users.noreply.github.com>
2022-11-23 10:05:48 +0330

    Add files via upload

---
+++ Akka.Network.Succinctly.Core.Common/CalculatorActor.cs
@@ -0,0 +1,63 @@
+﻿using Akka.Actor;
+
+namespace Akka.Network.Succinctly.Core.Common
+{
+
+    public class AddMessage
+
+    {
+
+        public AddMessage(double term1, double term2)
+
+        {
+
+            Term1 = term1;
+
+            Term2 = term2;
+
+        }
+
+        public double Term1;
+
+        public double Term2;
+
+    }
+
+    public class AnswerMessage
+
+    {
+
+        public AnswerMessage(double value)
+
+        {
+
+            Value = value;
+
+        }
+
+        public double Value;
+
+    }
+
+    public class CalculatorActor : ReceiveActor
+
+    {
+
+        public CalculatorActor()
+
+        {
+
+            Receive<AddMessage>(add =>
+
+            {
+
+                Console.WriteLine($"{DateTime.Now}: Sum {add.Term1} + {add.Term2}");
+
+                Sender.Tell(new AnswerMessage(add.Term1 + add.Term2));
+
+            });
+
+        }
+
+    }
+}
---
+++ Akka.Network.Succinctly.Core.Common/HoconLoader.cs
@@ -0,0 +1,26 @@
+﻿using Akka.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Akka.Network.Succinctly.Core.Common
+{
+    public static class HoconLoader
+
+    {
+
+        public static Config FromFile(string path)
+
+        {
+
+            var hoconContent = System.IO.File.ReadAllText(path);
+
+            return ConfigurationFactory.ParseString(hoconContent);
+
+        }
+
+    }
+
+}
