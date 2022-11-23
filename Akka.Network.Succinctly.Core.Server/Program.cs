a118d6dadac29716b4da7831b66b6690c8a992a8
Ehsan Akbar <45014174+akbar-ehsaan@users.noreply.github.com>
2022-11-23 10:04:48 +0330

    Add files via upload

---
+++ Akka.Network.Succinctly.Core.Server/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Akka.Actor;
+using Akka.Network.Succinctly.Core.Common;
+
+Console.WriteLine("Hello, World!");
+var hocon = HoconLoader.FromFile("akka.net.hocon.conf");
+
+ActorSystem system = ActorSystem.Create("server-system", hocon);
+
+Console.WriteLine("Server started");
+
+Console.Read();
+
+system.Terminate().Wait();
